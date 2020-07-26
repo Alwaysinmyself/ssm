9e7f0922e5c27b8b11af5084ce96ed2ed6142209
AlwaysInMySelf <1214890574@qq.com>
2020-07-26 16:55:18 +0800

    去掉了登录功能

--- jdbctest/src/jdbc_test.java
+++ jdbctest/src/jdbc_test.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 
 public class jdbc_test {
 
-    //主函数，程序的入口
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入用户名");
-        String username = sc.nextLine();
-        System.out.println("请输入密码");
-        String password = sc.nextLine();
-
-        boolean loginFlag = new jdbc_test().login(username, password);
-        if(loginFlag){
-            System.out.println("登录成功");
-        }else {
-            System.out.println("登陆失败");
-        }
-    }
+//    //主函数，程序的入口
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("请输入用户名");
+//        String username = sc.nextLine();
+//        System.out.println("请输入密码");
+//        String password = sc.nextLine();
+//
+//        boolean loginFlag = new jdbc_test().login(username, password);
+//        if(loginFlag){
+//            System.out.println("登录成功");
+//        }else {
+//            System.out.println("登陆失败");
+//        }
+//    }
 
     public boolean login(String username, String password){
         if(username==null || password==null){
